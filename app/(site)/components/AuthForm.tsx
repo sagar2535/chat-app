99c61beb0cfb419f727d4e8fd5ef6d0f485a77ce
sagar <sagarkumar2535@gmail.com>
2023-06-01 20:40:00 +0530

    initial-commit

---
+++ app/(site)/components/AuthForm.tsx
@@ -0,0 +1,11 @@
+'use client';
+
+import { useState } from 'react';
+
+const AuthForm = () => {
+  const [variant, setVariant] = useState();
+
+  return <div>AuthForm</div>;
+};
+
+export default AuthForm;
