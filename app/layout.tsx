c87c322d873e83956228e25c157235734527fb90
Sagar Kumar <93446599+sagar2535@users.noreply.github.com>
2023-07-20 15:33:18 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,9 +17,9 @@ export default function RootLayout({
     <html lang="en">
       <body>
         <AuthContext>
-          <ToasterContext />
           <ActiveStatus />
           {children}
+          <ToasterContext />
         </AuthContext>
       </body>
     </html>
